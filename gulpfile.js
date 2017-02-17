f368932ef548b3707d27293da2206fe391ccd8bc
andela-mugorji <munachimso.ugorji@andela.com>
2017-02-17 09:23:00 +0100

    chore(eslint): ammended eslint.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,20 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create;
+
+gulp.task('serve', () => {
+  browserSync({
+    server: {
+      baseDir: './src'
+    }
+  });
+});
+
+gulp.task('watch-html', browserSync.reload);
+
+
+// Default task
+gulp.task('watch', ['serve', 'watch-html'], () => {
+  gulp.watch('./src/*.html', ['watch-html']);
+  gulp.watch('./src/css/*.css', ['watch-html']);
+  gulp.watch('./src/js/*.js', ['watch-html']);
+});
