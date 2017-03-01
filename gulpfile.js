b1f9a2a87985d0622b7ea73a085bbaef48f1d753
andela-mugorji <munachimso.ugorji@andela.com>
2017-03-01 17:22:47 +0100

    chore(package): revised for heroku

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync');
 
 gulp.task('browserSync', () => {
-	browserSync.init({
-		server: {
-			baseDir: './src'
-		}
-	});
+  browserSync.init({
+    server: {
+      baseDir: './src'
+    }
+  });
 });
 
 gulp.task('watch', ['browserSync'], () => {
-	gulp.watch('src/*.html', browserSync.reload);
-	gulp.watch('src/css/*.css', browserSync.reload);
-	gulp.watch('src/js/*.js', browserSync.reload);
+  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('src/css/*.css', browserSync.reload);
+  gulp.watch('src/js/*.js', browserSync.reload);
 });
