1d00ed4c6e2727fb6a5e70264fdc0fd221cc8aa0
andela-mugorji <munachimso.ugorji@andela.com>
2017-03-05 14:55:25 +0100

    chore(js): added more methods to invertedindex class

--- src/js/invertedindex.js
+++ src/js/invertedindex.js
@@ -49,4 +49,55 @@ class InvertedIndex {
     }
     return check;
   }
+
+  createIndex(file, fileContent) {
+    const allWords = [];
+    for (const word of fileContent) {
+      const title = word.title;
+      const text = word.text;
+      const joinwords = title + ' ' + text;
+      allWords.push(this.removeDoubleWords(joinwords.split(' ')));
+    }
+    this.storeIndex(file, allWords);
+  }
+
+  storeIndex(file, allWords) {
+    const wordIndex = {};
+    for (const item in allWords) {
+      let itemToInt = parseInt(item);
+      allWords[item].forEach((word) => {
+        if (wordIndex[word]) {
+          if (wordIndex[word].indexOf(itemToInt) === -1) {
+            wordIndex[word].push(itemToInt);
+          }
+        } else {
+          wordIndex[word] = [itemToInt];
+        }
+      });
+    }
+    this.index[file] = wordIndex;
+  }
+
+  getIndex(file) {
+    return this.index[file];
+  }
+
+  search(fileName, word) {
+    const searchResult = {};
+    const cleanUpWord = this.cleanup(word);
+    const files = this.index;
+
+    searchResult[fileName] = {};
+    cleanUpWord.forEach(() => {
+      if (files[fileName][word]) {
+        searchResult[fileName][word] = files[fileName][word];
+      } else {
+        searchResult[fileName][word] = [];
+      }
+    });
+    return searchResult;
+  }
+
+
+
 }
